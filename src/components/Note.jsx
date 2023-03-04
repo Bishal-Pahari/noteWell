7c235c748cf6c0a18dac6dac993b3ded6acaf240
Bishal Pahari <Bishal.pahari53@gmail.com>
2023-03-04 11:30:52 +0545

    Fixed UI design issues

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,47 +2,50 @@ import React from "react";
 import { useState } from "react";
 export default function Note(props) {
   const [noteObj, setNoteObj] = useState({
-    title:props.title,
-    content:props.content
+    title: props.title,
+    content: props.content,
   });
   const handleClick = () => {
     props.onDelete(props.id);
   };
 
-  const handleChangeForTitle =(event)=>{
-    console.log("title changing")
-    setNoteObj((prevNote)=>{
-      return(
-        {
-          ...prevNote,
-          title: event.target.value
-        }
-      )
+  const handleChangeForTitle = (event) => {
+    console.log("title changing");
+    setNoteObj((prevNote) => {
+      return {
+        ...prevNote,
+        title: event.target.value,
+      };
     });
     console.log(noteObj);
 
-     props.onUpdate(props.id,noteObj);
-  }
+    props.onUpdate(props.id, noteObj);
+  };
 
-  const handleChangeForContent =(event)=>{
-    setNoteObj((prevNote)=>{
-      return(
-        {
-          ...prevNote,
-          content: event.target.value
-        }
-      )
+  const handleChangeForContent = (event) => {
+    setNoteObj((prevNote) => {
+      return {
+        ...prevNote,
+        content: event.target.value,
+      };
     });
-    props.onUpdate(props.id,noteObj);
-  }
+    props.onUpdate(props.id, noteObj);
+  };
   return (
     <div className="note">
-      <input value={noteObj.title}  onChange={handleChangeForTitle} className="note__title"/>
-      
-      <input value={noteObj.content}  onChange={handleChangeForContent} className="note__content"/>
-      
+      <input
+        value={noteObj.title}
+        onChange={handleChangeForTitle}
+        className="note__title"
+      />
+
+      <input
+        value={noteObj.content}
+        onChange={handleChangeForContent}
+        className="note__content"
+      />
+
       <button onClick={handleClick}>Delete</button>
     </div>
   );
 }
-

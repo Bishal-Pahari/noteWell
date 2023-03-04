33d95482d98923d1669cc5cc9d44a9a0e9c02358
yujen555 <ujen555@gmail.com>
2023-03-04 10:28:48 +0545

    crud operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,62 @@
 import React, { useEffect } from "react";
-
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 import { useState } from "react";
-import { auth,db } from "./firebase";
-import { onValue, ref, set,get,child } from "firebase/database";
+import { auth, db } from "./firebase";
+import { onValue, ref, set, get, child } from "firebase/database";
 
-export default function App({profileURL,profileName,userId}) {
-
-
-
+export default function App({ profileURL, profileName, userId }) {
   const [notes, setNotes] = useState([]);
-   
+
+  useEffect(() => {
+    const notesRef = ref(db, `users/${userId}/notes`);
+    onValue(notesRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        setNotes(Object.values(data));
+      }
+    });
+  }, [userId]);
+
   const addNote = (newNote) => {
     setNotes((prevNotes) => {
-      return [...prevNotes, newNote];
+      const updatedNotes = [...prevNotes, newNote];
+      // Update Firebase with the new note
+      set(ref(db, `users/${userId}/notes`), updatedNotes);
+      return updatedNotes;
     });
- 
-  
   };
-  useEffect(()=>{
-    set(ref(db, 'users/' + userId), {
-      notes:notes
-    });
-    
-  });
-
-
-
-
 
   const deleteNote = (id) => {
     setNotes((prevNotes) => {
-      return prevNotes.filter((noteItem, index) => {
+      const updatedNotes = prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
+      // Update Firebase with the updated notes list
+      set(ref(db, `users/${userId}/notes`), updatedNotes);
+      return updatedNotes;
     });
   };
 
-    useEffect(() => {
-    onValue( ref(db, 'users/' + userId), (snapshot) => {
-      const data = snapshot.val();
-      if(data != null){
-          setNotes( Object.values(data.notes));
-      }
+  const updateNote = (id, updatedNote) => {
+    setNotes((prevNotes) => {
+      const updatedNotes = notes.map((noteItem, index) => {
+        if (index == id) {
+          return updatedNote;
+        } else {
+          return noteItem;
+        }
+      });
+      set(ref(db, `users/${userId}/notes`), updatedNotes);
+      return(updatedNotes);
     });
-  },[]);
-
+  };
 
   return (
     <div>
-      <Header profileURL={profileURL} profileName={profileName}/>
+      <Header profileURL={profileURL} profileName={profileName} />
       <CreateArea onAdd={addNote} />
       <div className="note-area">
         {notes.map((noteItem, index) => {
@@ -63,13 +67,11 @@ export default function App({profileURL,profileName,userId}) {
               title={noteItem.title}
               content={noteItem.content}
               onDelete={deleteNote}
+              onUpdate={updateNote}
             />
           );
         })}
       </div>
-
-
-
       <Footer />
     </div>
   );
